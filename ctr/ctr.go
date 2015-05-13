f977a684044b5f81dde9b33f3648b45902672348
Juan F. Giménez Silva <juanfgs@gmail.com>
2015-05-13 16:40:23 -0300

    cleaned up code on encryption and decryption

--- ctr/ctr.go
+++ ctr/ctr.go
@@ -7,10 +7,6 @@ import (
 	"crypto/cipher"
 	"bytes"
 	"github.com/juanfgs/hmwk_crypto/tools"
-
-	"math"
-	
-
 )
 
 type CTR struct {
@@ -27,7 +23,6 @@ func New(key []byte) CTR {
 
 // Encrypt function takes a plain text returns an AES/CBC encrypted CT
 func (this CTR) Encrypt(pt []byte) []byte {
-
 	var iv []byte = make([]byte, 16)
 	_, err := rand.Read(iv)
 
@@ -36,24 +31,21 @@ func (this CTR) Encrypt(pt []byte) []byte {
 	}
 	CT := iv 
 
-	
-
-
 	max := len(pt) / aes.BlockSize
 	rest := len(pt) % aes.BlockSize
 	
 	for i:=0; i <= max ; i++ {
 
 		if i == max {
-			CT = append(CT,tools.XOR( GrabBlock(pt, rest, i * aes.BlockSize)  , this.chainBlock(iv) )...)
+			CT = append(CT,tools.XOR( GrabBlock(pt, rest, i * aes.BlockSize)  , this.EncryptBlock(iv) )...)
 		} else {
-			CT = append(CT,tools.XOR( GrabBlock(pt, aes.BlockSize, i * aes.BlockSize)  , this.chainBlock(iv) )...)
+			CT = append(CT,tools.XOR( GrabBlock(pt, aes.BlockSize, i * aes.BlockSize)  , this.EncryptBlock(iv) )...)
 		}
 		iv[len(iv) -1] = iv[len(iv) -1] + 1		
 	}
 
 	return bytes.TrimSuffix(CT, []byte{0})
-//	return CT
+
 }
 
 
@@ -62,44 +54,30 @@ func (this CTR) Decrypt(ct []byte) []byte {
 	iv := ct[:aes.BlockSize]
 
 	CT := ct[aes.BlockSize:]
+	var PT []byte
 
-	PT := tools.XOR(this.chainBlock(iv),GrabBlock(CT, aes.BlockSize, 0))
 	max := len(CT) / aes.BlockSize
 	rest := len(CT) % aes.BlockSize
-	for i:=1; i < max; i++ {
+	
+	for i:=0; i <= max; i++ {
+		if i == max {
+			PT = append(PT,tools.XOR( GrabBlock(CT, rest, i * aes.BlockSize )  , this.EncryptBlock(iv) ) ...)
+		} else {
+			PT = append(PT,tools.XOR( GrabBlock(CT, aes.BlockSize, i * aes.BlockSize )  , this.EncryptBlock(iv) ) ...)
+		}
 		iv[len(iv) -1] = iv[len(iv) -1 ] + 1
-		PT = append(PT,tools.XOR( GrabBlock(CT, aes.BlockSize, i * aes.BlockSize )  , this.chainBlock(iv) ) ...)
 	}
-	iv[len(iv) -1] = iv[len(iv) -1 ] + 1	
-	PT = append(PT,tools.XOR( GrabBlock(CT, rest , max * aes.BlockSize )  , this.chainBlock(iv) ) ...)
+
 	return PT
 }
 
 
 
-func (this CTR) deChainBlock(ct []byte) []byte {
+func (this CTR) EncryptBlock(ct []byte) []byte {
 	var pt []byte = make([]byte, aes.BlockSize)
-	this.Cipher.Decrypt(pt, ct)
+	this.Cipher.Encrypt(pt, ct)
 
 	return pt
-}
-
-func (this CTR) chainBlock(src []byte) []byte {
-	var ct []byte = make([]byte, aes.BlockSize)
-
-	this.Cipher.Encrypt(ct, src)
-	return ct
-}
-
-// Pads to fit blocksize
-
-func Pad(origin []byte, blockSize int) []byte {
-	bitblocks := int(math.Ceil(float64(len(origin)) / float64(16)))
-
-	var dst []byte = make([]byte, bitblocks*blockSize)
-	copy(dst, origin)
-
-	return dst
 }
 
 // Grabs a block from the full input, specifying  block size and initial index
